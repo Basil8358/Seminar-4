505c9b12ae9b1c5d2ff58875356f3cea10ac7d05
Basil8358 <106601905+Basil8358@users.noreply.github.com>
2022-06-21 14:03:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// Console.WriteLine("Hello, World!");
+// Напишите программу, 
+
+// void GetDot( int x, int y){
+//     if ( x == 0 || y == 0){
+//         Console.WriteLine("Нет возможности определит четверть");
+//     }else{
+//         if(x > 0 && y > 0)
+//             Console.WriteLine("1 четверть");
+//       if(x > 0 && y < 0 )
+//       Console.WriteLine("4 четверть");
+
+
+//       if(x < 0 && y > 0)
+//             Console.WriteLine("2 четверть");
+
+
+//       if(x < 0 && y < 0 )
+//       Console.WriteLine("4 четверть");
+
+//     }
+//     }
+// Console.WriteLine("введите две точки ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// int y1 = Convert.ToInt32(Console.ReadLine());
+
+// GetDot(x1, y1);
+
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти(x и y).
+    
+// void  getDot (int num)
+// {
+//  if (num == 1)
+//     Console.WriteLine("x > 0, y > 0");
+// else if (num == 2)
+//     Console.WriteLine("x < 0, y > 0");
+// else if (num == 3)
+//     Console.WriteLine("x < 0, y < 0");
+// else if (num == 4)
+//     Console.WriteLine("x > 0, y < 0");
+// else {
+//             Console.WriteLine("введено некорретное число ");}
+// } Console.WriteLine("введите номер четверти");
+//  int a = Convert.ToInt32(Console.ReadLine());
+
+//  getDot(a);
